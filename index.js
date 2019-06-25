25d44783812e0e1ea3e5228e95e9ee30d33afdaf
sinduja <sindhu.38@gmail.com>
2019-06-25 03:46:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function takeANumber(katzDeli,name)
     katzDeli.push(name);
   }
 
-  console.log("Welcome," +name+ ". You are number" +katzDeli.length+ "in line.");
+return "Welcome," +name+ ". You are number" +katzDeli.length+ "in line.";
 }
