4a734be7da28c64fdd2a46993ae2a35b34d578ec
sinduja <sindhu.38@gmail.com>
2019-06-25 03:55:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function takeANumber(katzDeli,name)
 
 return "Welcome, " +name+ ". You are number " +katzDeli.length+ " in line.";
 }
+
+function nowServing(katzDeli)
+{
+  if(katzDeli.length==0)
+  {
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    var name=katzDeli[0];
+    katzDeli.shift();
+    return "Currently serving "+name+" .";
+  }
+}
