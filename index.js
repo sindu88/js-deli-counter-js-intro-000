3b5b10740b0f8fadd43576098a6276884cf4fa66
sinduja <sindhu.38@gmail.com>
2019-06-25 04:07:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,19 @@ function nowServing(katzDeli)
     return "Currently serving "+name+".";
   }
 }
+
+function currentLine(katzDeli)
+{
+    var newArray;
+  if(katzDeli.length==0)
+  {
+    return "The line is currently empty.";
+  }
+  else {
+    for(let i=0;i<katzDeli.length;i++)
+    {
+    newArray.push(`$i+1` + "."+ katzDeli[i])
+    }
+    return "The line is currently: "+newArray+;
+  }
+}
