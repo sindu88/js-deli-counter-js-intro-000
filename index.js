bb10dd0359a0a3ddce75a8823d23884067f834f4
sinduja <sindhu.38@gmail.com>
2019-06-25 03:47:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function takeANumber(katzDeli,name)
     katzDeli.push(name);
   }
 
-return "Welcome," +name+ ". You are number" +katzDeli.length+ "in line.";
+return "Welcome, " +name+ ". You are number " +katzDeli.length+ "in line.";
 }
